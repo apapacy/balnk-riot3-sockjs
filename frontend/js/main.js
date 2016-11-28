6d1532a7c9e294b1344407acd1e28dfbd5967744
Андрей Овчаренко <ovcharenkoav@meta.ua>
2016-11-29 01:09:52 +0200

    react

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -4,5 +4,6 @@ import 'services';
 import 'events';
 import 'riot/frontend-tag';
 import 'riotcontrol'
+import 'react/test'
 
 riot.mount('body', 'layout-tag', {});
---
+++ frontend/js/react/test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+alert(3)
+console.log(ReactDOM)
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('root')
+);
+alert(4)
