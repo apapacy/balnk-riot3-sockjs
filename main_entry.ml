5758e602af390537a3943684fb430932954e1942
ovcharenkoav <ovcharenkoav@meta.ua>
2016-12-19 21:17:13 +0200

    couchbase/ottoman

--- main_entry.ml
+++
@@ -1,6 +0,0 @@
-#mod_use home/apapacy/projects/bucklescript-addons/bindings/bs-promise/src/bs_promise.mli
-open Bs_promise
-
-let	()	=		for	i	=	0	to	10	do
-  Js.log	(Fib.fib	i)
-done
---
+++ src/main_entry.ml
@@ -0,0 +1,3 @@
+let  ()  =    for  i  =  0  to  10  do
+  Js.log  (Fib.fib  i)
+done
