93608aa7d64b8e12768d6fb5c9a49a89808892bf
Андрей Овчаренко <ovcharenkoav@meta.ua>
2016-12-20 02:44:40 +0200

    couchbaser ref

--- app/models/index.js
+++ app/models/index.js
@@ -1,33 +1,23 @@
+//select p.body, p.title, u.name from default p  join default u on keys 'User|' || p.`user`.`$ref`;
+
+import  {promify} from '../utils/util';
 var ottoman = require('ottoman');
 var couchbase = require('couchbase');
 var cluster = new couchbase.Cluster('couchbase://127.0.0.1');
 ottoman.bucket = cluster.openBucket('default');
 
 
-function PhoneValidator(val) {
-  var phoneno = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
-  if(val && !val.match(phoneno)) {
-    throw new Error('Phone number is invalid.');
-  }
-}
-var Furniture = ottoman.model('Furniture', {
-  name: 'string',
-  frame: 'Date'
+var Posts = ottoman.model('Post', {
+  user: {ref: 'User'},
+  title: 'string',
+  body: 'string',
 }, {
   index: {
-    findByName: {
-      by: 'name'
+    findByUser: {
+      by: 'user',
+      type: 'view'
     }
   }
-});
-Furniture.prototype.dance = function() {
-  console.log('I am furniture, I do not dance.');
-};
-
-var Posts = ottoman.model('Post', {
-  user: {ref:'User'},
-  title: 'string',
-  body: 'string'
 });
 
 var Users = ottoman.model('User', {
@@ -41,125 +31,17 @@ var Users = ottoman.model('User', {
   }
 });
 
-var Employees = ottoman.model('Employee', {
-  name: 'string'
-}, {
-  queries: {
-    myPosts: {
-      of: 'Post',
-      by: 'user'
-    }
-  }
-});
-
-
-var CustomerMdl = ottoman.model('Customer', {
-  customerID: { type:'string', auto:'uuid', readonly:true }, // ← auto-increment UUID
-  createdON: { type:'Date', default:new Date() },            // ← auto populate date field
-  name: {                                                    // ← embedded string document
-    first: 'string',
-    last: 'string'
-  },
-  address: {                                                 // ← embedded address document
-    street: 'string',
-    city: 'string',
-    state: 'string',
-    zip: 'integer',
-    country: { type:'string', default:'USA' }                // ← auto populate string
-  },
-  phone: { type:'string', validator:PhoneValidator },        // ← string using custom validator
-  email: 'string',
-  history: [{                                                // ← array of historical events
-    date: { type: 'Date', default:new Date() },
-    employee: 'Employee',                                    // ← reference to another ottoman object
-    interaction: 'string',
-    notes: 'string'
-  }],
-  active: { type:'boolean', default:true }
-},{
-  index: {
-    findByCustomerID: {            // ← refdoc index
-      by: 'customerID',
-      type: 'refdoc'
-    },
-    findByEmail: {                 // ← refdoc index
-      by: 'email',
-      type: 'refdoc'
-    },
-    findByFirstName: {             // ← secondary index
-      by: 'name.first'
-    },
-    findByLastName: {              // ← secondary index
-      by: 'name.last'
-    }
-  }
-});
-
-
-CustomerMdl.createAndSave = function (firstname, lastname, addrStreet, addrCity, addrState, addrZip, email, done) {
-  this.create({
-    name: {
-      first: firstname,
-      last: lastname
-    },
-    address: {
-      street: addrStreet,
-      city: addrCity,
-      state: addrState,
-      zip: addrZip
-    },
-    email: email
-  }, done);
-};
-
-CustomerMdl.createAndSave('firstname', 'lastname', 'addrStreet', 'addrCity', 'addrState', 'addrZip', 'email', function(err, data) {
-  if (err) {
-    console.log('*********************')
-    console.log(err)
-  }
-  console.log(data);
+promify(ottoman, ottoman.ensureIndices)
+.then(data => {
+  var user = new Users({name: 'John'});
+  return promify(user, user.save);
 })
-
-var user = new Users({name: 'Joe'});
-var post = new Posts({user, title: 'title++', body: 'body++' })
-
-post.save(
-  function(err, data){
-    user.save(function(){});
-  }
+.then(user=>{
+  var post = new Posts({user, title: 'title', body: 'body'});
+  return promify(post, post.save)
+})
+.then(post=> promify(Users, Users.myPosts, {}))
+.then(
+  (posts=>console.log(posts)),
+  (err => console.log(err))
 );
-
-var couch = new Furniture({name:'Couch'});
-console.log(couch.name); // 'Couch'
-
-var table = new Furniture({name:'Table', frame: new Date()});
-table.dance();
-
-table.save(function(err) {
-  if (err) return console.error(err);
-  table.dance();
-});
-
-ottoman.ensureIndices(function(err) {
-  if (err) return console.error(err);
-});
-
-Furniture.findByName('Table', function(err, tables) {
-  if (err) return console.error(err);
-  console.log(tables);
-})
-
-
-Posts.find({}, {},function(err, data){
-  console.log('++++++++++++++++++++++')
-  if (err) {
-    console.log(err)
-  }
-  data[0].user.load(function(err, data0){
-    console.log('-------------------')
-    if (err) {
-      console.log(err)
-    }
-    console.log(data)
-  })
-})
--- app/utils/util.js
+++ app/utils/util.js
@@ -16,3 +16,23 @@ export function prepareQueryParameters(parameters) {
     }
     return query;
 }
+
+export function promify(self, func, ...args) {
+    return new Promise(function(resolve, reject) {
+        // node.js object.method(...args, callback)
+        // callback is last element of arguments list
+        // callbcak signature callbcak(error, data)
+        args.push(function(error, data) {
+            if (error) {
+                reject(error);
+            } else {
+                if (data) {
+                  resolve(data);
+                } else {
+                  resolve(self);
+                }
+            }
+        });
+        func.apply(self, args);
+    });
+}
