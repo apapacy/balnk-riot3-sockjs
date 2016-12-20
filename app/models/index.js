e52590a1ce5e6666aa54bc6362a7961e72c101fc
Андрей Овчаренко <ovcharenkoav@meta.ua>
2016-12-20 03:07:41 +0200

    couchbaser ref

--- app/models/index.js
+++ app/models/index.js
@@ -1,9 +1,10 @@
-//select p.body, p.title, u.name from default p  join default u on keys 'User|' || p.`user`.`$ref`;
+//select p.body, p.title, u.name from default p  join default u on keys 'User|' || p.`user`.`$ref`  where u._type='User' and p._type='Post';
+;
 
 import  {promify} from '../utils/util';
 var ottoman = require('ottoman');
 var couchbase = require('couchbase');
-var cluster = new couchbase.Cluster('couchbase://127.0.0.1');
+var cluster = new couchbase.Cluster('couchbase://127.0.0.1?operation_timeout=1000&config_total_timeout=1000&http_poolsize=100');
 ottoman.bucket = cluster.openBucket('default');
 
 
@@ -33,8 +34,11 @@ var Users = ottoman.model('User', {
 
 promify(ottoman, ottoman.ensureIndices)
 .then(data => {
-  var user = new Users({name: 'John'});
-  return promify(user, user.save);
+  for (let i = 0; i < 100000; i++) {
+    var user = new Users({name: 'John' + i});
+    var test = promify(user, user.save);
+  }
+  return test;
 })
 .then(user=>{
   var post = new Posts({user, title: 'title', body: 'body'});
